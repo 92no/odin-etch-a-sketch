f8338f7f118dd14ca685cb1b7b78d64deeaedcf8
Novak Nikolic <155026416+92no@users.noreply.github.com>
2024-03-14 12:27:35 +0100

    Edit index.html, script.js and style.css
    Add Stylesheet, buttons and code for grid colors

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 "use strict";
 
+// Function to return random number up to 255
+function randomNum() {
+  return Math.floor(Math.random() * 256);
+}
+
 // sketch grid 16x16 divs
 const GRID_WIDTH = 800;
 const sketchContainer = document.querySelector(".sketchContainer");
+let squaresPerSide = 16;
 
-function createGrid(squaresPerSide) {
+// Create grid with mono colors
+function createGridMono(squaresPerSide) {
   for (let i = 0; i < squaresPerSide ** 2; i++) {
     const sketchDiv = document.createElement("div");
     sketchDiv.classList.add("sketchDiv");
@@ -17,9 +24,23 @@ function createGrid(squaresPerSide) {
   }
 }
 
-function changeGrid() {
+// Create grid with random colors
+function createGridRandom(squaresPerSide) {
+  for (let i = 0; i < squaresPerSide ** 2; i++) {
+    const sketchDiv = document.createElement("div");
+    sketchDiv.classList.add("sketchDiv");
+    sketchDiv.style.height = `${GRID_WIDTH / squaresPerSide}px`;
+    sketchDiv.style.width = `${GRID_WIDTH / squaresPerSide}px`;
+    sketchDiv.addEventListener("mouseover", () => {
+      sketchDiv.style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    });
+    sketchContainer.appendChild(sketchDiv);
+  }
+}
+
+function getSquaresPerSide() {
   let squaresPerSide = prompt("Enter squares per Side (up to 100)");
-  while (squaresPerSide > 100) {
+  while (squaresPerSide > 100 || squaresPerSide == 0 || squaresPerSide == "") {
     squaresPerSide = prompt("Enter squares per Side (up to 100)");
   }
   return squaresPerSide;
@@ -32,11 +53,20 @@ function clearGrid() {
   });
 }
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", () => {
+// Button create Grid with mono colors
+const btnMono = document.querySelector(".btnMono");
+btnMono.addEventListener("click", () => {
   clearGrid();
-  let squaresPerSide = changeGrid();
-  createGrid(squaresPerSide);
+  let squaresPerSide = getSquaresPerSide();
+  createGridMono(squaresPerSide);
 });
 
-createGrid(16);
+// Btton create Grid with random colors
+const btnRandom = document.querySelector(".btnRandom");
+btnRandom.addEventListener("click", () => {
+  clearGrid();
+  let squaresPerSide = getSquaresPerSide();
+  createGridRandom(squaresPerSide);
+});
+
+createGridMono(squaresPerSide);
